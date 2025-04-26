ccc2dc592445ee7e06ccfcf7a78d0accb1d89ffc
Banx87 <joni.lindholm@gmail.com>
2025-04-26 12:27:15 +0300

    [MOD] Separated master template to own section files. Changed route from layout file to home index.

---
+++ resources/views/frontend/pages/home/index.blade.php
@@ -0,0 +1,103 @@
+@extends('frontend.layouts.master')
+@section('content')
+    <!--===========================
+            HERO 3 START
+        ============================-->
+
+    @include('frontend.pages.home.sections.hero')
+
+    <!--===========================
+            HERO 3 END
+        ============================-->
+
+
+    <!--===========================
+            CATEGORY 4 START
+        ============================-->
+
+    @include('frontend.pages.home.sections.categories')
+
+    <!--===========================
+            CATEGORY 4 END
+        ============================-->
+
+    <!--===========================
+            ABOUT 3 START
+        ============================-->
+    @include('frontend.pages.home.sections.about')
+    <!--===========================
+            ABOUT 3 END
+        ============================-->
+
+
+    <!--===========================
+            COURSES 3 START
+            ============================-->
+    @include('frontend.pages.home.sections.courses')
+    <!--===========================
+            COURSES 3 END
+        ============================-->
+
+
+    <!--===========================
+            OFFER START
+        ============================-->
+    @include('frontend.pages.home.sections.offer')
+    <!--===========================
+            OFFER END
+        ============================-->
+
+
+    <!--===========================
+            BECOME INSTRUCTOR START
+        ============================-->
+    @include('frontend.pages.home.sections.become-instructor')
+    <!--===========================
+            BECOME INSTRUCTOR END
+        ============================-->
+
+
+    <!--===========================
+            VIDEO START
+        ============================-->
+    @include('frontend.pages.home.sections.video')
+    <!--===========================
+            VIDEO END
+        ============================-->
+
+
+    <!--===========================
+            BRAND START
+        ============================-->
+    @include('frontend.pages.home.sections.brand')
+    <!--===========================
+            BRAND END
+        ============================-->
+
+
+    <!--===========================
+            FEATURED COURSES START
+        ============================-->
+    @include('frontend.pages.home.sections.featured-courses')
+    <!--===========================
+            FEATURED COURSES END
+        ============================-->
+
+
+    <!--===========================
+            TESTIMONIAL START
+        ============================-->
+    @include('frontend.pages.home.sections.testimonials')
+    <!--===========================
+            TESTIMONIAL END
+        ============================-->
+
+
+    <!--===========================
+            BLOG-CAROUSEL 4 START
+        ============================-->
+    @include('frontend.pages.home.sections.blog-carousel')
+    <!--===========================
+            BLOG-CAROUSEL 4 END
+        ============================-->
+@endsection
