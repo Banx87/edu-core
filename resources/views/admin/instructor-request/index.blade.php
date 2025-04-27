4d78230f96375ddaae73279bda4cbf51d8fbf7eb
Banx87 <joni.lindholm@gmail.com>
2025-04-27 16:24:36 +0300

    [ADD] Implement Instructor Request management with index, add corresponding view and sidebar link

---
+++ resources/views/admin/instructor-request/index.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="row row-deck row-cards">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Card Title</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-vcenter card-table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Role</th>
+                                        <th>Status</th>
+                                        <th>Document</th>
+                                        <th>Action</th>
+                                        <th class="w-1"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($instructorsRequest as $instructor)
+                                        <tr>
+                                            <td>{{ $instructor->name }}</td>
+                                            <td class="text-secondary">
+                                                <a href="#" class="text-reset">{{ $instructor->email }}</a>
+                                            </td>
+                                            <td class="text-secondary">
+                                                {{ $instructor->role }}
+                                            </td>
+                                            <td class="text-secondary">
+                                                @if ($instructor->approve_status === 'pending')
+                                                    <span class="badge bg-yellow text-yellow-fg">Pending</span>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <a href="#" class="text-secondary" style="margin-left: 1rem;"><svg
+                                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="icon icon-tabler icons-tabler-outline icon-tabler-download">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+                                                        <path d="M7 11l5 5l5 -5" />
+                                                        <path d="M12 4l0 12" />
+                                                    </svg></i></a>
+                                            </td>
+                                            <td class="text-secondary">
+                                                <form action="">
+                                                    <select name="" id="" class="form-control">
+                                                        <option value="1">Approve</option>
+                                                        <option value="2">Reject</option>
+                                                        <option value="3">Pending</option>
+                                                    </select>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="3">No data available!</td>
+                                        </tr>
+                                    @endforelse
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\InstructorRequestController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["middleware" => "guest:admin", "prefix" => "admin", "as" => "admin."], function () {
@@ -56,4 +57,6 @@ Route::group(["middleware" => "auth:admin", "prefix" => "admin", "as" => "admin.
 
     Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
+
+    Route::resource('instructor-request', InstructorRequestController::class);
 });
