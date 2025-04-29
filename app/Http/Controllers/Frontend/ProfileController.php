5d962853fc785d98e29c275e0ccf14aae5dd859f
Banx87 <joni.lindholm@gmail.com>
2025-04-29 16:42:23 +0300

    [ADD] Implement ProfileController and ProfileUpdateRequest for user profile management

---
+++ app/Http/Controllers/Frontend/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class ProfileController extends Controller
+{
+    function index(): View
+    {
+        return view('frontend.student-dashboard.profile.index');
+    }
+
+    function profileUpdate(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->headline = $request->headline;
+        $user->bio = $request->about;
+        $user->gender = $request->gender;
+        $user->save();
+
+        return Redirect::route('student.profile.index')->with('success', 'Profile updated successfully.');
+    }
+}
