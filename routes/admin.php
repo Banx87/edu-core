87ff30ebf413a1768b526f8c95eace72a892bc09
Banx87 <joni.lindholm@gmail.com>
2025-05-02 15:35:20 +0300

    [ADD] Add routes for CourseSubCategory management; implement store and index methods

--- routes/admin.php
+++ routes/admin.php
@@ -75,5 +75,7 @@ Route::group(["middleware" => "auth:admin", "prefix" => "admin", "as" => "admin.
 
     // Course Categories Routes
     Route::resource('course-categories', CourseCategoryController::class);
+    Route::post('{course_category}/sub-categories', [CourseSubCategoryController::class, 'store'])->name('course-sub-categories.store');
     Route::get('{course_category}/sub-categories', [CourseSubCategoryController::class, 'index'])->name('course-sub-categories.index');
+    Route::get('{course_category}/sub-categories/create', [CourseSubCategoryController::class, 'create'])->name('course-sub-categories.create');
 });
