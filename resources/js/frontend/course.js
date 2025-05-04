1884cc45a49ab4a0bbd9c414655480bc47b74c29
Banx87 <joni.lindholm@gmail.com>
2025-05-04 14:48:47 +0300

    [ADD] Implement course form submission handling and AJAX requests for creating and updating courses

---
+++ resources/js/frontend/course.js
@@ -0,0 +1,66 @@
+const base_url = $(`meta[name="base_url"]`).attr("content");
+const basic_info_url = base_url + "/instructor/courses/create";
+const update_url = base_url + "/instructor/courses/update";
+
+$(".basic_info_form").on("submit", function (e) {
+	e.preventDefault();
+	let formData = new FormData(this);
+
+	$.ajax({
+		method: "POST",
+		url: basic_info_url,
+		data: formData,
+		contentType: false,
+		processData: false,
+		beforeSend: function () {},
+		success: function (data) {
+			if (data.status === "success") {
+				window.location.href = data.redirect;
+			}
+		},
+		error: function (xhr, status, error) {},
+		complete: function () {},
+	});
+});
+
+$(".more_info_form").on("submit", function (e) {
+	e.preventDefault();
+	let formData = new FormData(this);
+
+	$.ajax({
+		method: "POST",
+		url: update_url,
+		data: formData,
+		contentType: false,
+		processData: false,
+		beforeSend: function () {},
+		success: function (data) {
+			if (data.status == "success") {
+				window.location.href = data.redirect;
+			}
+		},
+		error: function (xhr, status, error) {},
+		complete: function () {},
+	});
+});
+
+$(".basic_info_update_form").on("submit", function (e) {
+	e.preventDefault();
+	let formData = new FormData(this);
+
+	$.ajax({
+		method: "POST",
+		url: update_url,
+		data: formData,
+		contentType: false,
+		processData: false,
+		beforeSend: function () {},
+		success: function (data) {
+			if (data.status === "success") {
+				window.location.href = data.redirect;
+			}
+		},
+		error: function (xhr, status, error) {},
+		complete: function () {},
+	});
+});
