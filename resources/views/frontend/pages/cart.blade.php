e39532850c1163f564fcec139010f4c6d980a23d
Banx87 <joni.lindholm@gmail.com>
2025-05-12 13:33:49 +0300

    [ADD] Add empty cart image and update cart view for better user experience

--- resources/views/frontend/pages/cart.blade.php
+++ resources/views/frontend/pages/cart.blade.php
@@ -21,79 +21,105 @@
 
 
     <section class="wsus__cart_view mt_120 xs_mt_100 pb_120 xs_pb_100">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 wow fadeInUp">
-                    <div class="cart_list">
-                        <div class="table-responsive">
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th class="pro_img">Product</th>
-
-                                        <th class="pro_name"></th>
-
-                                        <th class="pro_tk">Price</th>
-
-
-                                        <th class="pro_icon">Remove</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($cart as $item)
+        @if (count($cart) > 0)
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 wow fadeInUp">
+                        <div class="cart_list">
+                            <div class="table-responsive">
+                                <table>
+                                    <thead>
                                         <tr>
-                                            <td class="pro_img">
-                                                <img src="{{ asset($item->course->thumbnail) }}" alt="product"
-                                                    class="img-fluid w-100">
-                                            </td>
-
-                                            <td class="pro_name">
-                                                <a
-                                                    href="{{ route('courses.show', $item->course->slug) }}">{{ $item->course->title }}</a>
-                                            </td>
-                                            <td class="pro_tk">
-                                                @if ($item->course->discount > 0)
-                                                    <h6>
-                                                        <del>
-                                                            <h6>${{ $item->course->price }}</h6>
-                                                        </del> ${{ $item->course->discount }}
-                                                    </h6>
-                                                @else
-                                                    <h6>${{ $item->course->price }}</h6>
-                                                @endif
-                                            </td>
-
-                                            <td class="pro_icon">
-                                                <a href="{{ route('remove-from-cart', $item->id) }}"><i class="fal fa-times"
-                                                        aria-hidden="true"></i></a>
-                                            </td>
+                                            <th class="pro_img">Product</th>
+                                            <th class="pro_name"></th>
+                                            <th class="pro_tk">Price</th>
+                                            <th class="pro_icon">Remove</th>
                                         </tr>
-                                    @empty
-                                        <p>Nothing added to cart</p>
-                                    @endforelse
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($cart as $item)
+                                            <tr>
+                                                <td class="pro_img">
+                                                    <img src="{{ asset($item->course->thumbnail) }}" alt="product"
+                                                        class="img-fluid w-100">
+                                                </td>
+
+                                                <td class="pro_name">
+                                                    <a
+                                                        href="{{ route('courses.show', $item->course->slug) }}">{{ $item->course->title }}</a>
+                                                </td>
+                                                <td class="pro_tk">
+                                                    @if ($item->course->discount > 0)
+                                                        <h6>
+                                                            <del>
+                                                                <h6>${{ $item->course->price }}</h6>
+                                                            </del> ${{ $item->course->discount }}
+                                                        </h6>
+                                                    @else
+                                                        <h6>${{ $item->course->price }}</h6>
+                                                    @endif
+                                                </td>
+
+                                                <td class="pro_icon">
+                                                    <a href="{{ route('remove-from-cart', $item->id) }}"><i
+                                                            class="fal fa-times" aria-hidden="true"></i></a>
+                                                </td>
+                                            </tr>
+                                            <td>
+
+                                                {{-- <div class="text-center">
+                                                    <img src="{{ asset('default_files/empty_cart.png') }}"
+                                                        alt="cart is empty" class="img-fluid">
+                                                    <p>Nothing added to cart</p>
+                                                </div>
+                                            </td>
+                                            <div class="row justify-content-between">
+                                                <div class="col-xxl-7 col-md-5 col-lg-6 wow fadeInUp"
+                                                    style="visibility: visible; animation-name: fadeInUp;">
+                                                    <div class="continue_shopping">
+                                                        <a href="#" class="common_btn">continue shopping</a>
+                                                    </div>
+                                                </div>
+                                            </div> --}}
+                                            @empty
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-between">
+                    <div class="col-xxl-7 col-md-5 col-lg-6 wow fadeInUp"
+                        style="visibility: visible; animation-name: fadeInUp;">
+                        <div class="continue_shopping">
+                            <a href="#" class="common_btn">continue shopping</a>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-md-7 col-lg-6 wow fadeInUp"
+                        style="visibility: visible; animation-name: fadeInUp;">
+                        <div class="total_price">
+                            <div class="subtotal_area">
+                                <h5>Total<span>${{ cartTotal() }}</span></h5>
+                                <a href="checkout.html" class="common_btn">proceed checkout</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-between">
+        @else
+            <div class="container text-center">
+                <img src="{{ asset('default_files/empty_cart.png') }}" alt="cart is empty" class="img-fluid"
+                    style="width: 180px !important">
+                <h5>No items in cart.</h5>
                 <div class="col-xxl-7 col-md-5 col-lg-6 wow fadeInUp"
                     style="visibility: visible; animation-name: fadeInUp;">
                     <div class="continue_shopping">
                         <a href="#" class="common_btn">continue shopping</a>
                     </div>
                 </div>
-                <div class="col-xxl-4 col-md-7 col-lg-6 wow fadeInUp"
-                    style="visibility: visible; animation-name: fadeInUp;">
-                    <div class="total_price">
-                        <div class="subtotal_area">
-                            <h5>Total<span>${{ cartTotal() }}</span></h5>
-                            <a href="checkout.html" class="common_btn">proceed checkout</a>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </div>
+        @endif
+
     </section>
 @endsection
