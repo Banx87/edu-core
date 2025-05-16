1d8cd7597f4f7a355aefe123b0b372870b517ead
Banx87 <joni.lindholm@gmail.com>
2025-05-16 12:20:50 +0300

    [ADD] Create OrderController and implement index method for order listing

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    function index(): View
+    {
+        $orders = Order::with(['customer'])->paginate(25);
+        $currencySymbols = getCurrencySymbols();
+        return view('admin.order.index', compact('orders'), compact('currencySymbols'));
+    }
+}
