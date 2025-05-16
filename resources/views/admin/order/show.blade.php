a7e0e215de13939ce5fe37b9beb5fd65c3256831
Banx87 <joni.lindholm@gmail.com>
2025-05-16 13:31:38 +0300

    [UPDATE] Refactor invoice display in order show view and enhance print functionality

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -11,26 +11,15 @@
                     </div>
                     <!-- Page title actions -->
                     <div class="col-auto ms-auto d-print-none">
-                        <button type="button" class="btn btn-primary" onclick="javascript:window.print();">
-                            <!-- Download SVG icon from http://tabler.io/icons/icon/printer -->
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="icon icon-1" data-darkreader-inline-stroke=""
-                                style="--darkreader-inline-stroke: currentColor;">
-                                <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2">
-                                </path>
-                                <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4"></path>
-                                <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z">
-                                </path>
-                            </svg>
+                        <button type="button" class="btn btn-primary" onclick="javascript:printPageBody();">
+                            <i class="ti ti-printer space"></i>
                             Print Invoice
                         </button>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- END PAGE HEADER -->
-        <!-- BEGIN PAGE BODY -->
+
         <div class="page-body">
             <div class="container-xl">
                 <div class="card card-lg">
@@ -48,14 +37,15 @@
                             <div class="col-6 text-end">
                                 <p class="h3">Client</p>
                                 <address>
-                                    Street Address<br>
-                                    State, City<br>
-                                    Region, Postal Code<br>
-                                    ctr@example.com
+                                    {{ $order->customer->name }}<br>
+                                    {{-- Street Address<br> --}}
+                                    {{-- State, City<br> --}}
+                                    {{-- Region, Postal Code<br> --}}
+                                    {{ $order->customer->email }}
                                 </address>
                             </div>
                             <div class="col-12 my-5">
-                                <h1>Invoice INV/001/15</h1>
+                                <h1>Invoice #{{ $order->invoice_id }}</h1>
                             </div>
                         </div>
                         <table class="table table-transparent table-responsive">
@@ -64,109 +54,55 @@
                                     <th class="text-center" style="width: 1%"></th>
                                     <th>Product</th>
                                     <th class="text-center" style="width: 1%">Qnt</th>
-                                    <th class="text-end" style="width: 1%">Unit</th>
-                                    <th class="text-end" style="width: 1%">Amount</th>
+                                    <th class="text-end" style="width: 2%">Amount</th>
                                 </tr>
                             </thead>
+                            @php
+                                $currency = strtoupper($order->currency);
+                                $currencySymbol = $currencySymbols[$currency] ?? '';
+                            @endphp
                             <tbody>
+                                @foreach ($order->orderItems as $item)
+                                    <tr>
+                                        <td class="text-center">1</td>
+                                        <td>
+                                            <p class="strong mb-1">{{ $item->course->title }}</p>
+                                            <div class="text-secondary">By {{ $item->course->instructor->name }}</div>
+                                        </td>
+                                        <td class="text-center">{{ $item->quantity }}</td>
+                                        <td class="text-end">{{ $currencySymbol }}{{ $item->price }}</td>
+                                    </tr>
+                                @endforeach
+
                                 <tr>
-                                    <td class="text-center">1</td>
-                                    <td>
-                                        <p class="strong mb-1">Logo Creation</p>
-                                        <div class="text-secondary">Logo and business cards design</div>
-                                    </td>
-                                    <td class="text-center">1</td>
-                                    <td class="text-end">$1.800,00</td>
-                                    <td class="text-end">$1.800,00</td>
+                                    <td colspan="3" class="strong text-end">Subtotal</td>
+                                    <td class="text-end">{{ $currencySymbol }}{{ $order->total_amount }}</td>
                                 </tr>
                                 <tr>
-                                    <td class="text-center">2</td>
-                                    <td>
-                                        <p class="strong mb-1">Online Store Design &amp; Development</p>
-                                        <div class="text-secondary">Design/Development for all popular modern browsers</div>
-                                    </td>
-                                    <td class="text-center">1</td>
-                                    <td class="text-end">$20.000,00</td>
-                                    <td class="text-end">$20.000,00</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">3</td>
-                                    <td>
-                                        <p class="strong mb-1">App Design</p>
-                                        <div class="text-secondary">Promotional mobile application</div>
-                                    </td>
-                                    <td class="text-center">1</td>
-                                    <td class="text-end">$3.200,00</td>
-                                    <td class="text-end">$3.200,00</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="4" class="strong text-end">Subtotal</td>
-                                    <td class="text-end">$25.000,00</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="4" class="strong text-end">Vat Rate</td>
-                                    <td class="text-end">20%</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="4" class="strong text-end">Vat Due</td>
-                                    <td class="text-end">$5.000,00</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="4" class="font-weight-bold text-uppercase text-end">Total Due</td>
-                                    <td class="font-weight-bold text-end">$30.000,00</td>
+                                    <td colspan="3" class="strong text-end">Paid Amount</td>
+                                    <td class="text-end">{{ $currencySymbol }}{{ $order->paid_amount }}
+                                        {{ $order->currency }}</td>
                                 </tr>
                             </tbody>
                         </table>
-                        <p class="text-secondary text-center mt-5">Thank you very much for doing business with us. We look
-                            forward to working with you again!</p>
+                        <p class="text-secondary text-center mt-5">We appreciate your business and look forward to serving
+                            you again!</p>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- END PAGE BODY -->
-        <!--  BEGIN FOOTER  -->
-        <footer class="footer footer-transparent d-print-none">
-            <div class="container-xl">
-                <div class="row text-center align-items-center flex-row-reverse">
-                    <div class="col-lg-auto ms-lg-auto">
-                        <ul class="list-inline list-inline-dots mb-0">
-                            <li class="list-inline-item"><a href="https://tabler.io/docs" target="_blank"
-                                    class="link-secondary" rel="noopener">Documentation</a></li>
-                            <li class="list-inline-item"><a href="./license.html" class="link-secondary">License</a></li>
-                            <li class="list-inline-item">
-                                <a href="https://github.com/tabler/tabler" target="_blank" class="link-secondary"
-                                    rel="noopener">Source code</a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="https://github.com/sponsors/codecalm" target="_blank" class="link-secondary"
-                                    rel="noopener">
-                                    <!-- Download SVG icon from http://tabler.io/icons/icon/heart -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="icon text-pink icon-inline icon-4" data-darkreader-inline-stroke=""
-                                        style="--darkreader-inline-stroke: currentColor;">
-                                        <path
-                                            d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572">
-                                        </path>
-                                    </svg>
-                                    Sponsor
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-lg-auto mt-3 mt-lg-0">
-                        <ul class="list-inline list-inline-dots mb-0">
-                            <li class="list-inline-item">
-                                Copyright © 2025
-                                <a href="." class="link-secondary">Tabler</a>. All rights reserved.
-                            </li>
-                            <li class="list-inline-item">Generated 2025-05-04 11:01 +0000</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!--  END FOOTER  -->
     </div>
 @endsection
+
+
+<script>
+    // Don't print the header portion, just the Invoice
+    function printPageBody() {
+        var printContent = document.querySelector('.page-body').innerHTML;
+        var originalContent = document.body.innerHTML;
+
+        document.body.innerHTML = printContent;
+        window.print();
+        document.body.innerHTML = originalContent;
+    }
+</script>
