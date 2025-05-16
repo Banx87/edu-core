5329e31c5aef15e6791e7ecad77170897ff38bbe
Banx87 <joni.lindholm@gmail.com>
2025-05-16 12:38:17 +0300

    [UPDATE] Add action buttons for viewing orders in the order index view

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -20,6 +20,7 @@
                                         <th>Paid Amount</th>
                                         <th>Currency</th>
                                         <th>Status</th>
+                                        <th>Actions</th>
                                         <th class="w-1"></th>
                                     </tr>
                                 </thead>
@@ -59,31 +60,9 @@
                                                 @endif
                                             </td>
                                             <td>
-                                                {{-- <a href="{{ route('admin.course-orders.edit', $order->id) }}"
-                                                    class="btn-sm btn-primary"><svg width="24" height="24"
-                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                        class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path
-                                                            d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-                                                        <path
-                                                            d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-                                                        <path d="M16 5l3 3" />
-                                                    </svg></a>
-                                                <a href="{{ route('admin.course-orders.destroy', $order->id) }}"
-                                                    class="btn-sm text-danger delete-item"><svg width="24"
-                                                        height="24" viewBox="0 0 24 24" fill="none"
-                                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path d="M4 7l16 0" />
-                                                        <path d="M10 11l0 6" />
-                                                        <path d="M14 11l0 6" />
-                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                                                    </svg></a> --}}
+                                                <a href="{{ route('admin.orders.show', $order->id) }}"
+                                                    class="btn-sm btn-primary"><i class="ti ti-eye"></i></a>
+
                                             </td>
                                         </tr>
                                     @empty
