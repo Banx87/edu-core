0d87eac7aa60fbc9181f565c8d831d7f6277e073
Banx87 <joni.lindholm@gmail.com>
2025-06-06 13:28:39 +0300

    [FIX] Removed Extra  "

--- resources/views/components/input-file-block.blade.php
+++ resources/views/components/input-file-block.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-3">
     <label for="{{ $name }}" class="form-label text-capitalize">{{ $label ?? $name }}</label>
-    <input type="file" class="form-control" name="{{ $name }}"">
+    <input type="file" class="form-control" name="{{ $name }}">
     <x-input-error :messages="$errors->get($name)" class="mt-2" />
 
 </div>
