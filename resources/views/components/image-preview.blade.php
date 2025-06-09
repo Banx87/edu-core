fd9c63fbd3c16e1f2b13da9f6219889602468e48
Banx87 <joni.lindholm@gmail.com>
2025-06-09 13:19:04 +0300

    [MOD] Update label rendering in image preview and input file block components for better null handling

--- resources/views/components/image-preview.blade.php
+++ resources/views/components/image-preview.blade.php
@@ -1,5 +1,7 @@
 <div class="mb-3" style="display: flex; flex-direction: column;">
-    <label for="imagePreview" class="form-label text-capitalize">{{ $label }}</label>
+    @if (isset($label))
+        <label for="imagePreview" class="form-label text-capitalize">{{ $label }}</label>
+    @endif
     <img {{ $attributes->merge(['class' => 'img-fluid']) }} {{-- src="{{ $src }}" alt="{{ $alt }} --}} "
         id="imagePreview" name="imagePreview" style="max-width: 150px; margin-bottom: 25px; height: auto; object-fit: cover;">
 </div>
